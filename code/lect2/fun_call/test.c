3861682550f0af274eb3c5224926373b0407f2d5
sunnysky29 <dufei_29@qq.com>
2023-11-25 12:50:04 +0000

    1125 add

---
+++ code/lect2/fun_call/test.c
@@ -0,0 +1,21 @@
+/*
+
+函数调用例子, 
+研究栈帧
+
+*/
+
+int bar(int c, int d)
+{
+    int e = c + d;
+    return e;
+}
+int foo(int a, int b)
+{
+    return bar(a, b);
+}
+int main(void)
+{
+    foo(2, 5);
+    return 0;
+}
--- code/lect2/hello.c
+++
@@ -1,8 +0,0 @@
-// #include <stdio.h>
-
-int main() {
-    printf("hello world7777\n");
-    // return 0;
-    // while (1);
-}
-
