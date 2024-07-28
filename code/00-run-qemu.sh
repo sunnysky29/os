29b7ab18cc2f4d32497257659c93c573c0e920fa
sunnysky29 <dufei_29@qq.ocm>
2024-07-28 21:51:01 +0800

    调试固件

--- code/00-run-qemu.sh
+++ code/00-run-qemu.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# https://www.bilibili.com/video/BV1HN41197Ko/?p=4&vd_source=abeb4ad4122e4eff23d97059cf088ab4
-
+# 视频 和 ppt:
+#        https://www.bilibili.com/video/BV1HN41197Ko/?p=4&vd_source=abeb4ad4122e4eff23d97059cf088ab4
+#        https://jyywiki.cn/OS/2021/slides/C2.slides.html#/3/6
+# 调试 firmware 
 # bash 00-run-qemu.sh   hello.img
 
 
